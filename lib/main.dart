1194a23eacad33c7a6e5e05f4c0ed975053341b6
71405245 <71405245@continental.edu.pe>
2025-09-25 18:08:12 -0500

    Resolviendo conflicto y reestructurando carpetas

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-import 'Infraestructura/firebase_options.dart';
-import 'Infraestructura/firestore_login.dart'; // importa tu login nuevo
+import 'firebase_options.dart';
+import 'firestore_login.dart'; // importa tu login nuevo
 
 
 Future<void> main() async {
